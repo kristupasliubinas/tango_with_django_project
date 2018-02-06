b8413a035d0149d73eaabcc712984df1b1991280
Kristupas Liubinas <2270674l@student.gla.ac.uk>
2018-02-06 23:02:07 +0000

    Chapter 5 + Exercises

--- populate_rango.py
+++ populate_rango.py
@@ -27,15 +27,15 @@ def populate():
     # This might seem a little bit confusing, but it allows us  to iterate
     # through each data structure, and add the data to our models.
 
-    cats = { "Python": {"pages": python_pages}, \
-             "Django": {"pages": django_pages}, \
-             "Other Frameworks": {"pages": other_pages} }
+    cats = { "Python": {"pages": python_pages, "views": 128, "likes": 64}, \
+             "Django": {"pages": django_pages, "views": 64, "likes": 32}, \
+             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
     
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.iteritems():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -52,8 +52,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
