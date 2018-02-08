72728e74839e6d104eff6ee1469dcde048aac47b
Kristupas Liubinas <2270674l@student.gla.ac.uk>
2018-02-08 12:45:45 +0000

    Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -10,18 +10,18 @@ def populate():
     # we want to add into each category.
 
     python_pages = [
-        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/"}, \
-        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/"}, \
-        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/"} ]
+        {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/", "views": 10}, \
+        {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/", "views": 100}, \
+        {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": 1000} ]
 
     django_pages = [
-        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"}, \
-        {"title": "Django Rocks", "url": "http://www.djangorocks.com/"}, \
-        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"} ]
+        {"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 20}, \
+        {"title": "Django Rocks", "url": "http://www.djangorocks.com/", "views": 200}, \
+        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/", "views": 2000} ]
 
     other_pages = [
-        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"}, \
-        {"title": "Flask", "url": "http://flask.pocoo.org"} ]
+        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 30}, \
+        {"title": "Flask", "url": "http://flask.pocoo.org", "views": 300} ]
 
     # Then we will create a dictionary of dictionaries for our categories.
     # This might seem a little bit confusing, but it allows us  to iterate
@@ -37,7 +37,7 @@ def populate():
     for cat, cat_data in cats.iteritems():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
@@ -45,7 +45,7 @@ def populate():
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- rango/urls.py
+++ rango/urls.py
@@ -4,4 +4,12 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    # This URL pattern will match a sequence of alphanumeric characters (\w)
+    # and hyphens (\-) which are between the rango/category/ and the trailing /.
+    # This sequence will be stored in the parameter category_- name_slug
+    # and passed to views.show_category().
+
+    # For example, the URL rango/category/python-books/ would result
+    # in the category_name_slug having the value python-books.
 ]
