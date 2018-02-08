d5478f46daf9e85fbafaaa7ca54411e5f98641c1
Kristupas Liubinas <2270674l@student.gla.ac.uk>
2018-02-08 23:20:17 +0000

    Chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,7 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     # This URL pattern will match a sequence of alphanumeric characters (\w)
     # and hyphens (\-) which are between the rango/category/ and the trailing /.
