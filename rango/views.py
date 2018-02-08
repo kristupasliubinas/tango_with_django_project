a6dddbee35c8ca5e9a257443100793a9eaec06a1
Kristupas Liubinas <2270674l@student.gla.ac.uk>
2018-02-08 13:25:26 +0000

    Chapter 6 + Exercises

--- rango/views.py
+++ rango/views.py
@@ -22,9 +22,11 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     category_list = Category.objects.order_by('-likes')[:5]
 
+    page_list = Page.objects.order_by('-views')[:5]
+
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
-    context_dict = {'categories': category_list}
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
