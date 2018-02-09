5d348a644eac44f39c59f06f25e29905f545d64d
Kristupas Liubinas <2270674l@student.gla.ac.uk>
2018-02-09 16:37:18 +0000

    Chapter 8 + Exercises

--- rango/views.py
+++ rango/views.py
@@ -37,7 +37,12 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    # prints out whether the method is a GET or POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints 'AnonymousUser'
+    print(request.user)
+    
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
